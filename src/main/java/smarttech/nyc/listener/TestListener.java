ae7867901b90cbfbd7015f9638eb3027c121b363
QA-Alam <alam.itprofessional@gmail.com>
2022-12-26 21:17:30 -0500

    Update pom

--- src/main/java/smarttech/nyc/listener/TestListener.java
+++ src/main/java/smarttech/nyc/listener/TestListener.java
@@ -1,8 +1,9 @@
 package smarttech.nyc.listener;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -15,21 +16,19 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.ChartLocation;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-
 public class TestListener implements ITestListener {
+
 	public ExtentHtmlReporter htmlReporter;
 	public ExtentReports extent;
 	public ExtentTest logger;
-	WebDriver driver;
-
-	@Override 
+	
+	@Override
 	public void onTestStart(ITestResult result) {
-		String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
+	String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());// time stamp
 		String repName = "report-" + timeStamp + ".html";
 
-		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/ExtendReports/" + repName);// specify
-																											// location
-		// of the report
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/ExtendReports/" + repName);// specify location
+																										// of the report
 		htmlReporter.loadXMLConfig(System.getProperty("user.dir") + "/extent-config.xml");
 
 		extent = new ExtentReports();
@@ -43,35 +42,53 @@ public class TestListener implements ITestListener {
 		htmlReporter.config().setReportName("Functional Test Report"); // name of the report
 		htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP); // location of the chart
 		htmlReporter.config().setTheme(Theme.DARK);
-
 	}
 
 	@Override
-	public void onTestSuccess(ITestResult result) {
-		logger = extent.createTest(result.getName()); // create new entry in th report
-		logger.log(Status.PASS, MarkupHelper.createLabel(result.getName(), ExtentColor.GREEN));
-	
-
-	}
-
-	@Override
-	public void onTestFailure(ITestResult result) {
-		logger = extent.createTest(result.getName()); // create new entry in th report
-		logger.log(Status.FAIL, MarkupHelper.createLabel(result.getName(), ExtentColor.RED)); // send the passed
+	public void onTestSuccess(ITestResult tr) {
+		logger = extent.createTest(tr.getName()); // create new entry in th report
+		logger.log(Status.PASS, MarkupHelper.createLabel(tr.getName(), ExtentColor.GREEN)); // send the passed
+			
 		
-	} 
+	}
 
 	@Override
-	public void onTestSkipped(ITestResult result) {
-		logger = extent.createTest(result.getName()); // create new entry in th report
-		logger.log(Status.SKIP, MarkupHelper.createLabel(result.getName(), ExtentColor.ORANGE));
+	public void onTestFailure(ITestResult tr) {
+		logger = extent.createTest(tr.getName()); // create new entry in th report
+		logger.log(Status.FAIL, MarkupHelper.createLabel(tr.getName(), ExtentColor.RED)); // send the passed information
+																							// to the report with GREEN
+																							// color highlighted
 
+		String screenshotPath = System.getProperty("user.dir") + "\\screenshot\\" + tr.getName() + ".png";
+
+		File f = new File(screenshotPath);
+
+		if (f.exists()) {
+			try {
+				logger.fail("Screenshot is below:" + logger.addScreenCaptureFromPath(screenshotPath));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult tr) {
+		logger = extent.createTest(tr.getName()); // create new entry in th report
+		logger.log(Status.SKIP, MarkupHelper.createLabel(tr.getName(), ExtentColor.ORANGE));
+		
 	}
 
 	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void onFinish(ITestContext context) {
+		extent.flush();
+
+		
 	}
 
 	@Override
@@ -80,10 +97,5 @@ public class TestListener implements ITestListener {
 		
 	}
 
-	@Override
-	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
